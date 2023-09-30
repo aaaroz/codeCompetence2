3e5f968f9ccf1d8c9b976cc130940dfb8a9c07e8
Muhamad Rhamdhan Mardiansyah <ram.ardiansyah18@gmail.com>
2023-09-30 17:57:48 +0700

    Initialized React App For Code Competence 2

---
+++ src/pages/landing.page.jsx
@@ -0,0 +1,31 @@
+import Contact from "../components/contact";
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default function LandingPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        {/* main header */}
+        <header>
+          <div className="welcome-text">
+            <h1 className="mb-2 lh-1 text-white">DIGITAL INNOVATION</h1>
+            <p className="mt-4 fs-5 fw-medium text-white">
+              Realize Your Innovation With Us!
+            </p>
+            <button
+              type="button"
+              className="btn btn-light fs-5 fw-medium ps-5 pe-5 mt-2 text-info"
+            >
+              Join Now
+            </button>
+          </div>
+        </header>
+        {/* end of main header */}
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+}
